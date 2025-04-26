886a7cc80f288e58611e5f58f55cf03b7596abe4
mphd123 <100949445+mphd123@users.noreply.github.com>
2025-04-26 19:18:32 +0200

    added support for getting a graph needs to be tested

--- src/main/java/co2/co2Server/database/controller/webcontroller.java
+++ src/main/java/co2/co2Server/database/controller/webcontroller.java
@@ -2,16 +2,25 @@ package co2.co2Server.database.controller;
 
 import co2.co2Server.database.Co2Entry;
 import co2.co2Server.database.SensorDataDB;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.sql.Date;
 import java.util.List;
 import java.util.NoSuchElementException;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtils;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
 
 @RestController
 public class webcontroller {
@@ -45,6 +54,45 @@ public class webcontroller {
         }
     }
 
+    @GetMapping("/image")
+    public ResponseEntity<byte[]> getImage() throws Exception {
+        List<Co2Entry> entries = db.getEntries();
+
+        XYSeries series = new XYSeries("CO₂ Measurements");
+
+        for (int i = 0; i < entries.size(); i++) {
+            series.add(i, entries.get(i).getCo2());
+        }
+
+        XYSeriesCollection dataset = new XYSeriesCollection(series);
+
+
+        JFreeChart chart = ChartFactory.createXYLineChart(
+                "CO₂ Measurements Over Time",
+                "Measurement Index (Hourly)",
+                "CO₂ (ppm)",
+                dataset
+        );
+
+        byte[] imageBytes;
+        try (var baos = new java.io.ByteArrayOutputStream()) {
+            ChartUtils.writeChartAsPNG(baos, chart, 800, 600);
+            imageBytes = baos.toByteArray();
+        }
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.IMAGE_PNG);
+
+        return new ResponseEntity<>(imageBytes, headers, HttpStatus.OK);
+    }
+
+
+
+    @GetMapping("/")
+    public String main2() throws Exception {
+            return "test";
+    }
+
     @PostMapping(path = "/addEntry")
     public ResponseEntity<Co2Entry> addEntry(@RequestBody Co2Entry entry) {
         try {
@@ -54,10 +102,5 @@ public class webcontroller {
         }
 
         return ResponseEntity.accepted().body(entry);
-    }
-
-    @GetMapping("/")
-    public String main2() throws Exception {
-            return "test";
     }
 }
